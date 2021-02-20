6fbbf668ead73d223324fcd68ffe86052c18f375
wilsonrivero <wilsonrivero2002@gmail.com>
2021-02-20 16:13:11 -0300

    add flash, add table in index page

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+app.secret_key = 'klnaçsd@asndk#asdk123'
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
 
--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -1,18 +1,23 @@
-from flask import render_template, request
+from flask import render_template, request, flash, redirect, url_for
 from app import app, db
 from app.models.tables import Products
 
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+   product = Products.query.all()
+   return render_template('index.html', products=product)
+
+
+
+
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
    if request.method == "POST":
       name = request.form['name']
       price = request.form['price']
-      price = float(price)
+     
       varejo = request.form.get('ok')
 
       if varejo == 'ok':
@@ -20,8 +25,18 @@ def register():
       else:
          varejo = False
 
-      product = Products(name, price , varejo)
-      db.session.add(product)
-      db.session.commit()
-      return render_template('index.html')
+      if name == '' or name == None:
+         flash('Por favor preencha o campo nome !')
+      elif price == '' or price == None:
+         flash('Erro por favor verifique o campo de preço')
+      else:
+         
+         flash('Cadastrado com sucesso')
+         price = float(price)
+         product = Products(name, price , varejo)
+         db.session.add(product)
+         db.session.commit()
+         return redirect(url_for('home'))
+
+
    return render_template('register.html')
