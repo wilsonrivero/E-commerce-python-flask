613dfbb4203c630a41d83203e8dbd9ec33937709
wilsonrivero <wilsonrivero2002@gmail.com>
2021-02-20 13:33:13 -0300

    add resgiter page, get data in routes

--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -1,7 +1,16 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 
 
 @app.route('/')
 def home():
    return render_template('index.html')
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+   if request.method == "POST":
+      name = request.form['name']
+      price = request.form['price']
+      print(f'\033[1;32m name: {name} price: {price} \033[1;32m')
+
+   return render_template('register.html')
